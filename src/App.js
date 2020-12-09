eef2cbf0ff2f57f1dc76ead109408d31ed267756
ederson <66290546+ederson-cinnamon@users.noreply.github.com>
2020-12-10 00:53:47 +0800

    simple increment

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import React, { Component } from 'react';
 class Index extends Component {
   state = { increment: 0 }
   render() {
+    var { increment } = this.state
     return (<div>
-      <p>{this.state.increment}</p>
+      <p>{increment}</p>
       <button onClick={() => {
-        this.setState({ increment: ++this.state.increment })
+        this.setState({ increment: ++increment })
       }}>Increment</button>
     </div>);
   }
