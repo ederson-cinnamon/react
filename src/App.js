5d6f2fbf05069d61a5f3ecd10ea6ee08519cf9f6
ederson <66290546+ederson-cinnamon@users.noreply.github.com>
2020-12-10 00:39:56 +0800

    initial time-stamp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Index extends Component {
+  state = { date: new Date() }
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>hello</h1>
+        <p>date today is {this.state.date.toLocaleTimeString()}</p>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Index;
